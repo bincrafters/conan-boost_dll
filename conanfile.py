1f8b6d0ccae6385564910e0b099a02ed6c3e8079
Jerry W <jerrywiltse@gmail.com>
2017-08-14 21:39:10 -0400

    mass update - minor recipe and cosmetic readme

--- conanfile.py
+++ conanfile.py
@@ -3,8 +3,8 @@ from conans import ConanFile, tools, os
 class BoostDllConan(ConanFile):
     name = "Boost.Dll"
     version = "1.64.0"
-    generators = "txt"
-    url = "https://github.com/boostorg/dll"
+    url = "https://github.com/bincrafters/conan-boost-dll"
+    source_url = "https://github.com/boostorg/dll"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "dll"
@@ -28,7 +28,7 @@ class BoostDllConan(ConanFile):
                       
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
-                 .format(self.version, self.url))
+                 .format(self.version, self.source_url))
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")

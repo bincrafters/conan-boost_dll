4fc1418b892f841f52f3e81825ced66dee9f1863
Rene Rivera <grafikrobot@gmail.com>
2017-12-20 21:14:31 -0600

    Initial 1.66.0 package branche.

--- conanfile.py
+++ conanfile.py
@@ -3,32 +3,32 @@ from conans import ConanFile
 
 class BoostDllConan(ConanFile):
     name = "Boost.Dll"
-    version = "1.65.1"
+    version = "1.66.0"
     requires = \
-        "Boost.Config/1.65.1@bincrafters/testing", \
-        "Boost.Core/1.65.1@bincrafters/testing", \
-        "Boost.Filesystem/1.65.1@bincrafters/testing", \
-        "Boost.Function/1.65.1@bincrafters/testing", \
-        "Boost.Move/1.65.1@bincrafters/testing", \
-        "Boost.Mpl/1.65.1@bincrafters/testing", \
-        "Boost.Predef/1.65.1@bincrafters/testing", \
-        "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
-        "Boost.Spirit/1.65.1@bincrafters/testing", \
-        "Boost.Static_Assert/1.65.1@bincrafters/testing", \
-        "Boost.System/1.65.1@bincrafters/testing", \
-        "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
-        "Boost.Type_Index/1.65.1@bincrafters/testing", \
-        "Boost.Type_Traits/1.65.1@bincrafters/testing", \
-        "Boost.Winapi/1.65.1@bincrafters/testing"
+        "Boost.Config/1.66.0@bincrafters/testing", \
+        "Boost.Core/1.66.0@bincrafters/testing", \
+        "Boost.Filesystem/1.66.0@bincrafters/testing", \
+        "Boost.Function/1.66.0@bincrafters/testing", \
+        "Boost.Move/1.66.0@bincrafters/testing", \
+        "Boost.Mpl/1.66.0@bincrafters/testing", \
+        "Boost.Predef/1.66.0@bincrafters/testing", \
+        "Boost.Smart_Ptr/1.66.0@bincrafters/testing", \
+        "Boost.Spirit/1.66.0@bincrafters/testing", \
+        "Boost.Static_Assert/1.66.0@bincrafters/testing", \
+        "Boost.System/1.66.0@bincrafters/testing", \
+        "Boost.Throw_Exception/1.66.0@bincrafters/testing", \
+        "Boost.Type_Index/1.66.0@bincrafters/testing", \
+        "Boost.Type_Traits/1.66.0@bincrafters/testing", \
+        "Boost.Winapi/1.66.0@bincrafters/testing"
     lib_short_names = ["dll"]
     is_header_only = True
 
     # BEGIN
 
     url = "https://github.com/bincrafters/conan-boost-dll"
-    description = "Please visit http://www.boost.org/doc/libs/1_65_1"
+    description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "www.boost.org/users/license.html"
-    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
+    build_requires = "Boost.Generator/1.66.0@bincrafters/testing"
     short_paths = True
     exports = "boostgenerator.py"
 

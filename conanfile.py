fcc627943e3fb3cbf73704a3cc735fc6f1c4be16
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:01:15 -0500

    change ver to 1.65.1 after merge

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class BoostDllConan(ConanFile):
     name = "boost_dll"
-    version = "1.66.0"
+    version = "1.65.1"
     url = "https://github.com/bincrafters/conan-boost_dll"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
@@ -17,22 +17,22 @@ class BoostDllConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_core/1.66.0@bincrafters/testing",
-        "boost_filesystem/1.66.0@bincrafters/testing",
-        "boost_function/1.66.0@bincrafters/testing",
-        "boost_move/1.66.0@bincrafters/testing",
-        "boost_mpl/1.66.0@bincrafters/testing",
-        "boost_predef/1.66.0@bincrafters/testing",
-        "boost_smart_ptr/1.66.0@bincrafters/testing",
-        "boost_spirit/1.66.0@bincrafters/testing",
-        "boost_static_assert/1.66.0@bincrafters/testing",
-        "boost_system/1.66.0@bincrafters/testing",
-        "boost_throw_exception/1.66.0@bincrafters/testing",
-        "boost_type_index/1.66.0@bincrafters/testing",
-        "boost_type_traits/1.66.0@bincrafters/testing",
-        "boost_winapi/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/testing",
+        "boost_config/1.65.1@bincrafters/testing",
+        "boost_core/1.65.1@bincrafters/testing",
+        "boost_filesystem/1.65.1@bincrafters/testing",
+        "boost_function/1.65.1@bincrafters/testing",
+        "boost_move/1.65.1@bincrafters/testing",
+        "boost_mpl/1.65.1@bincrafters/testing",
+        "boost_predef/1.65.1@bincrafters/testing",
+        "boost_smart_ptr/1.65.1@bincrafters/testing",
+        "boost_spirit/1.65.1@bincrafters/testing",
+        "boost_static_assert/1.65.1@bincrafters/testing",
+        "boost_system/1.65.1@bincrafters/testing",
+        "boost_throw_exception/1.65.1@bincrafters/testing",
+        "boost_type_index/1.65.1@bincrafters/testing",
+        "boost_type_traits/1.65.1@bincrafters/testing",
+        "boost_winapi/1.65.1@bincrafters/testing"
     )
 
     # BEGIN
@@ -40,7 +40,7 @@ class BoostDllConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()

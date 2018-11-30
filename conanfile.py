62d533500f82114b1003914270092ec9391bb295
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:11 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostDllConan(base.BoostBaseConan):
     name = "boost_dll"
@@ -28,5 +28,3 @@ class BoostDllConan(base.BoostBaseConan):
         "boost_type_traits",
         "boost_winapi"
     ]
-
-

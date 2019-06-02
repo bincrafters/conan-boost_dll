514b0d5ddac7d886423d1f0e3c4c5742f5fdc39c
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,28 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostDllConan(base.BoostBaseConan):
     name = "boost_dll"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_dll"
-    lib_short_names = ["dll"]
-    header_only_libs = ["dll"]
-    b2_requires = [
-        "boost_config",
-        "boost_core",
-        "boost_filesystem",
-        "boost_function",
-        "boost_move",
-        "boost_mpl",
-        "boost_predef",
-        "boost_smart_ptr",
-        "boost_spirit",
-        "boost_static_assert",
-        "boost_system",
-        "boost_throw_exception",
-        "boost_type_index",
-        "boost_type_traits",
-        "boost_winapi"
-    ]
+    version = "1.70.0"
